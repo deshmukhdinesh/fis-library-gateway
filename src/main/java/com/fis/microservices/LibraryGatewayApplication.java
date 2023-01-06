3d5d480887a898fa547743316dab9ab21c6fa44d
Dinesh Deshmukh <deshmukhdinesh00@gmail.com>
2023-01-06 09:47:13 +0530

    Gateway for library services

---
+++ src/main/java/com/fis/microservices/LibraryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.fis.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryGatewayApplication.class, args);
+	}
+
+}
